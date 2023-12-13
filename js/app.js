79c065c352596f1e52672766a4dfe1235a9977d3
CarlesNero <csanchezdsgn@gmail.com>
2023-12-13 23:33:01 +0100

    favicon change

--- js/app.js
+++ js/app.js
@@ -7,17 +7,42 @@ convert.addEventListener("click", function () {
   let fromCurrency = from.value;
   let toCurrency = to.value;
   let amt = amount.value;
-  
 
-  if (fromCurrency === "EUR" && toCurrency === "YEN") rate = 155.59;
+  if (!amt || isNaN(amt)) {
+    alert("Por favor, ingrese un número válido en el campo de cantidad.");
+    return;
+  }
+
+  /*   if (fromCurrency === "EUR" && toCurrency === "JPY") rate = 155.59;
   else if (fromCurrency === "EUR" && toCurrency === "USD") rate = 1.09;
   else if (fromCurrency === "USD" && toCurrency === "EUR") rate = 0.92;
-  else if (fromCurrency === "USD" && toCurrency === "YEN") rate = 143.02;
-  else if (fromCurrency === "YEN" && toCurrency === "EUR") rate = 0.0064;
-  else if (fromCurrency === "YEN" && toCurrency === "USD") rate = 0.007;
-  else rate = 1
+  else if (fromCurrency === "USD" && toCurrency === "JPY") rate = 143.02;
+  else if (fromCurrency === "JPY" && toCurrency === "EUR") rate = 0.0064;
+  else if (fromCurrency === "JPY" && toCurrency === "USD") rate = 0.007;
+  else rate = 1; */
+
+  const RATES = {
+    EUR: {
+      EUR: 1,
+      USD: 1.09,
+      JPY: 155.59,
+    },
+    USD: {
+      EUR: 0.92,
+      USD: 1,
+      JPY: 143.02,
+    },
+    JPY: {
+      EUR: 0.0064,
+      USD: 0.007,
+      JPY: 1,
+    },
+  };
+
+  const rate = RATES[fromCurrency][toCurrency];
 
   let total = rate * amt;
-  result.innerHTML = `${amt} ${fromCurrency} = ${total.toFixed(2)} ${toCurrency}`;
-
+  result.innerHTML = `${amt} ${fromCurrency} = ${total.toFixed(
+    2
+  )} ${toCurrency}`;
 });
